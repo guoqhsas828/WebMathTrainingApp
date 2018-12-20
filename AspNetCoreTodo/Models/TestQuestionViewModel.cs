ac43943eb6a97c2e7afd4381dac8cb508fa219c0
Joshua Guo <jguo@quantifisolutions.local>
2018-12-20 18:02:07 -0500

    start adding the test question

---
+++ AspNetCoreTodo/Models/TestQuestionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMathTraining.Models
+{
+    public class TestQuestionViewModel
+    {
+        public string Id { get; set; }
+
+        public TestCategory Category { get; set; }
+
+        [Required]
+        public int Level { get; set; }
+
+        [Display(Name = "Question Name")]
+        public string QuestionName { get; set; }
+
+        [Required]
+        public string QuestionId { get; set; }
+
+      public string StatusMessage { get; set; }
+
+  }
+}
