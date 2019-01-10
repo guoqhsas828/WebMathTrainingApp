7f5da65f9705a2690ffb0765ab9f590f69992de8
Joshua Guo <jguo@quantifisolutions.local>
2019-01-10 17:41:45 -0500

    fix the login issue when user name is different from email

--- AspNetCoreTodo/Models/AccountViewModels/LoginViewModel.cs
+++ AspNetCoreTodo/Models/AccountViewModels/LoginViewModel.cs
@@ -9,8 +9,8 @@ namespace WebMathTraining.Models.AccountViewModels
     public class LoginViewModel
     {
         [Required]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name="User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
